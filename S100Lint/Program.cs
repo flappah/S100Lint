985f6ed36f0f2e5ddfbf7d15eb0a672a07bb33ba
Richard Flapper <richard_flapper@hotmail.com>
2020-01-23 11:52:33 +0100

    Typo

--- S100Lint/Program.cs
+++ S100Lint/Program.cs
@@ -31,7 +31,7 @@ namespace S100Lint
                     reportItems = schemaParser.Validate(file1, file2);
                 }
 
-                Console.WriteLine("\General Information:");
+                Console.WriteLine("General Information:");
                 foreach (var reportItem in reportItems)
                 {
                     if (reportItem.Type == Types.Enumerations.Type.Info)
