dde4e4db0abac096ca9924d12c86dac8e36bd7e8
Richard Flapper <richard_flapper@hotmail.com>
2020-01-30 16:57:15 +0100

    Added documentation for command line option

--- S100Lint/Program.cs
+++ S100Lint/Program.cs
@@ -13,7 +13,9 @@ namespace S100Lint
         {
             if (args.Length < 2)
             {
-                Console.WriteLine("Invalid syntax for S100Lint. Valid syntax is S100Lint [SchemaFileName] [SchemaFileName | FeatureCatalogueFileName]");
+                Console.WriteLine(String.Format("Invalid syntax for S100Lint. Valid syntax is S100Lint [SchemaFileName] [SchemaFileName | FeatureCatalogueFileName] [options]\n\n" +
+                    "Options:\n " +
+                    "--fc: Checks if all the types included in the feature catalogue are defined in the XML schema."));
             }
             else 
             {
